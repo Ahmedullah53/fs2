b04e54017500dc6169f5b51cc2ef47dea659325d
AhmedullahSiddiqui <ahmedullah.siddiqui@tenpearls.com>
2019-11-26 17:52:28 +0500

    frontend and mongo setup

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Data from './components/data-list';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header classname="App-header">
+      	<p>Heading</p>
+      </header>
+      <Data/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/data-list.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Data(){
+   return (
+   	<div>
+	  <p>This is rendered from Data.</p>   
+	</div>
+   )
+}
+
+export default Data
