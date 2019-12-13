6b2840c1d603e2204deee7e61e2c6d0a51d9ade0
AhmedullahSiddiqui <ahmedullah.siddiqui@tenpearls.com>
2019-12-13 18:15:19 +0500

    WIP

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,55 @@
 import time
 
 import redis
-from flask import Flask
+from flask import Flask, jsonify, make_response, request
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
 @app.route('/')
 def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello World! I have been seen {} times.\n'.format(1)
 
+@app.route('/customer-location/', methods=['GET'])
+def customer_location():
+    response_body = {
+        "latitude": 24,
+        "longitude": 67
+    }
+    res = make_response(jsonify(response_body), 200)
+    return res
+
+@app.route('/customer-details/', methods=['GET'])
+def customer_details():
+    response_body = {
+        "name": "Ahmedullah Siddiqui",
+        "email": "ahmedullah.siddiqui@tenpearls.com",
+        "contact": "0322-9465316",
+    }
+    res = make_response(jsonify(response_body), 200)
+    return res
+
+@app.route('/driver-details/', methods=['GET'])
+def driver_details():
+    response_body = {
+        "name": "Ahmedullah Siddiqui",
+        "email": "ahmedullah.siddiqui@tenpearls.com",
+        "contact": "0322-8227433",
+        "vehicle": {
+            "plates": "ABQ-700",
+            "color": "green",
+            "make": "Suzuki",
+            "model": "Mehran",
+        }
+    }
+    res = make_response(jsonify(response_body), 200)
+    return res
+
+@app.route('/driver-location/', methods=['GET'])
+def driver_location():
+    response_body = {
+        "latitude": 24,
+        "longitude": 67
+    }
+    res = make_response(jsonify(response_body), 200)
+    return res
